cc547881d7b847c1db19d78738cf95c49b1ebd4b
DevVitorSilva <vitor.silva001080@gmail.com>
2024-02-14 16:08:53 -0300

    rows and columns of the created matrix

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("Enter a number of lines: ");
+        int lines = sc.nextInt();
+        System.out.print("Enter a number of columns: ");
+        int columns = sc.nextInt();
+        int[][] matrix = new int[lines][columns];
+        for(int il = 0; il < lines; il++){
+            for(int ic = 0; ic < columns; ic++){
+                matrix[il][ic] = sc.nextInt();
+            }
+        }
         sc.close();
     }
 }
